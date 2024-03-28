851d37f7d70fd1f5b1b5a0d4d731539727879e50
Louis Gambardella <LouisGambardella@gmail.com>
2024-03-28 11:48:59 -0700

    Java Script Exercise

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ console.log(
 
 // Exercise 1
 
+let lastName;
+let age;
+console.log (lastName);
 // Exercise 2
-
+firstName = "louis";
+lastName = "gambardella";
+age = 62;
+console.log (lastName);
 // Exercise 3
+let language = "English";
+let createdYear = 1995;
+let isCaseSensitive ="true";
+// Exercise 4
+let price = 19.99;
+let isOnSale = "false";
+let salePercentage = 15;
+let stock = 0
+let instock = "false";
+let selectedSize = "M";
+// Exercise 5
+let tilte = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = "true";
+console.log (instock);
+console.log (tilte);
